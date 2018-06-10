f6f670d4b0503e54a68d93d7b2f6f9323c7c1156
Ivan Zhur <vzhur98@gmail.com>
2018-06-10 17:39:44 +0300

    Fix find

--- lab6/src/find.java
+++ lab6/src/find.java
@@ -15,6 +15,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * A simple directory files and subdirectories listing program for a simulated file system.
@@ -74,72 +75,66 @@ public class find {
         }
         // if name is a directory open it and read the contents
         else {
-            // Print directory name
-            System.out.println(name);
+            Queue<String> files = new LinkedList<>();
+            files.add(name);
 
-            // open the directory
-            int fd = Kernel.open(name, Kernel.O_RDONLY);
-            if (fd < 0) {
-                Kernel.perror(PROGRAM_NAME);
-                System.err.println(PROGRAM_NAME + ": unable to open \"" + name + "\" for reading");
-                Kernel.exit(1);
-            }
+            while (!files.isEmpty()) {
+                name = files.poll();
+                // Print file name
+                System.out.println(name);
 
-            // Trim slash in directory name for fancy output
-            if (name.equals("/")) name = "";
-
-            // List to store subdirectories and then process them
-            // This is required for listing files first and then subdirectories
-            List<String> directories = new LinkedList<>();
-
-            // create a directory entry structure to hold data as we read
-            DirectoryEntry directoryEntry = new DirectoryEntry();
-
-            // while we can read, print the information on each entry
-            while (true) {
-                // read an entry; quit loop if error or nothing read
-                status = Kernel.readdir(fd, directoryEntry);
-                if (status <= 0) break;
-
-                // get the name from the entry
-                String entryName = directoryEntry.getName();
-
-                // Don not print . and .. dirs
-                if (entryName.equals(".") || entryName.equals("..")) continue;
-
-                // call stat() to get info about the file
-                String entryFullName = name + "/" + entryName;
-                status = Kernel.stat(entryFullName, stat);
+                // Get file info
+                status = Kernel.stat(name, stat);
                 if (status < 0) {
                     Kernel.perror(PROGRAM_NAME);
                     Kernel.exit(1);
                 }
-
                 // mask the file type from the mode
                 short currentType = (short) (stat.getMode() & Kernel.S_IFMT);
-                // If directory, save for processing
-                if (currentType == Kernel.S_IFDIR) {
-                    directories.add(entryFullName);
+                // If just a file, printing is enough
+                if (currentType != Kernel.S_IFDIR) continue;
+
+                // open the directory
+                int fd = Kernel.open(name, Kernel.O_RDONLY);
+                if (fd < 0) {
+                    Kernel.perror(PROGRAM_NAME);
+                    System.err.println(PROGRAM_NAME + ": unable to open \"" + name + "\" for reading");
+                    Kernel.exit(1);
                 }
-                // If simple entry, print the name
-                else {
-                    System.out.println(entryFullName);
+
+                // Trim slash in directory name for fancy output
+                if (name.equals("/")) name = "";
+
+                // create a directory entry structure to hold data as we read
+                DirectoryEntry directoryEntry = new DirectoryEntry();
+
+                // while we can read, add all files to queue
+                while (true) {
+                    // read an entry; quit loop if error or nothing read
+                    status = Kernel.readdir(fd, directoryEntry);
+                    if (status <= 0) break;
+
+                    // get the name from the entry
+                    String entryName = directoryEntry.getName();
+
+                    // Don not print . and .. dirs
+                    if (entryName.equals(".") || entryName.equals("..")) continue;
+
+                    // Add file to queue
+                    String entryFullName = name + "/" + entryName;
+                    files.add(entryFullName);
                 }
-            }
 
-            // check to see if our last read failed
-            if (status < 0) {
-                Kernel.perror("main");
-                System.err.println("main: unable to read directory entry from /");
-                Kernel.exit(2);
-            }
+                // check to see if our last read failed
+                if (status < 0) {
+                    Kernel.perror("main");
+                    System.err.println("main: unable to read directory entry from /");
+                    Kernel.exit(2);
+                }
 
-            for (String path : directories) {
-                processFile(path);
+                // close the directory
+                Kernel.close(fd);
             }
-
-            // close the directory
-            Kernel.close(fd);
         }
     }
 }
